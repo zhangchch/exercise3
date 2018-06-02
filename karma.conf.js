92e18e14f9dc444f48e2b9281215ee225b2a6f83
zhang_c <zhang_c@camobile.com>
2018-06-02 18:37:12 +0900

    travis config

--- karma.conf.js
+++ karma.conf.js
@@ -57,12 +57,24 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+
+
+    plugins: [
+      'karma-mocha',
+      'karma-chrome-launcher',
+    ],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
